e259ccf2c78a25f21c4ce81ee2f7333cb567656a
Archie P <archieinet@gmail.com>
2017-05-05 16:53:08 -0500

    add web api

---
+++ repository/db/CHIMS4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repository.db
+{
+    public class CHIMS4 : DbContext
+    {
+        public CHIMS4() : base("CHIMS4")
+        {}
+
+
+    }
+}
