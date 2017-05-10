e5a92a29c8be5354f0a0b3576cf130d1ac7fcfff
Archie P <archieinet@gmail.com>
2017-05-10 16:56:42 -0500

    updated

---
+++ WebUI.v1/Scripts/app-chi-ms4.js
@@ -0,0 +1,27 @@
+var app = angular.module('app-chims', [
+        'ui.router'
+]);
+
+
+(function () {
+    'use strict';
+
+    var controller = function () {
+
+
+    }; //controller
+
+
+
+
+
+
+
+    app.component('login', {
+            templateUrl: './views/login.html',
+            controller: controller,
+            controllerAs: 'login'
+
+        });
+
+})();
---
+++ WebUI.v1/app/components/login.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    var controller = function () {
+
+
+    }; //controller
+
+
+
+
+
+
+
+    app.component('login', {
+            templateUrl: './views/login.html',
+            controller: controller,
+            controllerAs: 'login'
+
+        });
+
+})();
