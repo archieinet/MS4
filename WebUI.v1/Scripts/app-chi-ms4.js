748c796169b81fa3ce561b20e86411a7cf8ae4ee
Archie P <archieinet@outlook.com>
2017-05-10 22:00:50 -0500

    set some samples

--- WebUI.v1/Scripts/app-chi-ms4.js
+++ WebUI.v1/Scripts/app-chi-ms4.js
@@ -3,6 +3,28 @@ var app = angular.module('app-chims', [
 ]);
 
 
+//--------delete all after this line
+app.provider('appname', function () {
+    this.$get = function () {
+
+        var appName = 'MS4.ORG',
+            appDesc = 'Upload shared file services';
+
+
+        return {
+            appName: appName,
+            appDesc:appDesc
+        };
+    };
+
+}); //app.provider
+
+app.controller('appCtrl', function(appname){
+    var vm = this;
+    vm.appName = appname.appName;
+    vm.appDesc = appname.appDesc;
+});
+
 (function () {
     'use strict';
 
--- WebUI.v1/app/app.js
+++ WebUI.v1/app/app.js
@@ -2,3 +2,25 @@
         'ui.router'
 ]);
 
+
+//--------delete all after this line
+app.provider('appname', function () {
+    this.$get = function () {
+
+        var appName = 'MS4.ORG',
+            appDesc = 'Upload shared file services';
+
+
+        return {
+            appName: appName,
+            appDesc:appDesc
+        };
+    };
+
+}); //app.provider
+
+app.controller('appCtrl', function(appname){
+    var vm = this;
+    vm.appName = appname.appName;
+    vm.appDesc = appname.appDesc;
+});
