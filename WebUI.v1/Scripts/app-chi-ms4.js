c15523eb6b74c41263959d80a82d3c505c12bcb8
Archie P <archieinet@gmail.com>
2017-05-26 16:11:20 -0500

    update load loop

--- WebUI.v1/Scripts/app-chi-ms4.js
+++ WebUI.v1/Scripts/app-chi-ms4.js
@@ -200,16 +200,28 @@ var app = angular.module('appMS4', [
 
         $scope.$on('selectedFile', function (event, args) {
             $scope.$apply(function () {
-                $scope.files.push(args.file);
-
+                    $scope.files.push(args.file);
             });//apply
-
-
         }); //$on
+        
         
         dash.removeAttach = function (idx) {
             $scope.files.splice(idx, 1);
         } //dash.removeAttach
+
+
+        dash.uploadFiles = function () {
+            if ($scope.files.length === 0) {
+                alert('No files to be upload');
+                return false;
+            }
+
+            //----upload here
+
+
+        }; //dash.uploadFiles();
+
+
 
         dash.profile = JSON.stringify($rootScope.Profile);
 
